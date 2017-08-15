6aa3c916a1ba885f01345f500e987c87787c79fa
lelukas <lelukass@gmail.com>
2017-08-15 15:12:52 -0300

    installed semantic on the project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+
+<div class="two columns grid">
+    <div class="column">Teste</div>
+    <div class="column">teste</div>
+</div>
+
+<script src="{{ URL::asset('semantic/semantic.js') }}"></script>
