25bdf6f86e00ddad719c561483d88447d6178fd4
lelukas <lelukass@gmail.com>
2017-08-16 00:30:46 -0300

    training view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,283 @@
-<link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}" />
-<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
 
-<div class="two columns grid">
-    <div class="column">Teste</div>
-    <div class="column">teste</div>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Academia Life Fitness</title>
+    <meta name=”viewport” content=”width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;” />
+    <link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+</head>
+
+<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+<link rel="stylesheet" href="css/css.css">
+<link rel="stylesheet" href="css/spinner.css">
+
+<div class="ui left demo thin vertical inverted sidebar labeled icon menu">
+    <a id="home" class="item">
+        <i class="checked calendar icon"></i>
+        Treino de Hoje
+    </a>
+    <a id="digievolution" class="item">
+        <i class="line chart icon"></i>
+        Minha Evolução
+    </a>
+    <a class="item">
+        <i class="setting icon"></i>
+        Configurações
+    </a>
+    <a id="quit" class="item">
+        <i class="power icon"></i>
+        Sair
+    </a>
 </div>
 
+<div class="pusher">
+
+    <div class="ui fixed inverted menu">
+        <div class="ui container">
+            <a id="menuButton" href="#" class="item circular ui icon button">
+                <i class="content icon"></i>
+            </a>
+            <a href="#" class="header item disabled">
+                <img class="ui mini circular image konata" src="images/maxresdefault.jpg">
+                <span id="top_name" class="ui label">Academia Life Fitness</span>
+            </a>
+        </div>
+    </div>
+
+    <section class="content">
+        <div class="ui transparent grey first main menu">
+            <div class="container">
+                <div class="borderless item">
+                    <img class="ui mini image" src="images/icon/chest.png">
+                    <div class="ui label ex">Peitoral</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <img class="ui mini image stats" src="images/icon/supino.png">
+                        <img class="ui mini image stats" src="images/icon/reset.png">
+                        <img class="ui mini image stats" src="images/icon/haltere.png">
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Supino Reto</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Supino Declinado</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Peck Deck</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">5</div>
+                        <div class="numbers secondrow">20</div>
+                        <div class="numbers thirdrow">2</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Apoio de Frente</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">4</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">1</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Pull Over</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">2</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">5</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ui transparent grey main menu">
+            <div class="container">
+                <div class="borderless item">
+                    <img class="ui mini image" src="images/icon/biceps.png">
+                    <div class="ui label ex">Tríceps</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <img class="ui mini image stats" src="images/icon/supino.png">
+                        <img class="ui mini image stats" src="images/icon/reset.png">
+                        <img class="ui mini image stats" src="images/icon/haltere.png">
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Puxador Inverso</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Tríceps Corda</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Tríceps Coice</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">5</div>
+                        <div class="numbers secondrow">20</div>
+                        <div class="numbers thirdrow">2</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Tríceps Francês</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">4</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">1</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Na Paralela</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">2</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">5</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ui transparent grey main menu">
+            <div class="container">
+                <div class="borderless item">
+                    <img class="ui mini image" src="images/icon/biceps.png">
+                    <div class="ui label ex">Ombro</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <img class="ui mini image stats" src="images/icon/supino.png">
+                        <img class="ui mini image stats" src="images/icon/reset.png">
+                        <img class="ui mini image stats" src="images/icon/haltere.png">
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Desenv. Haltere</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Cross Frontal</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">3</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">4</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Cross Lateral</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">5</div>
+                        <div class="numbers secondrow">20</div>
+                        <div class="numbers thirdrow">2</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Elev. da Escapula</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">4</div>
+                        <div class="numbers secondrow">10</div>
+                        <div class="numbers thirdrow">1</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container table items">
+                <div class="borderless item">
+                    <div class="ui label items">Remada Alta</div>
+                </div>
+                <div class="right menu">
+                    <div class="vertically fitted borderless item">
+                        <div class="numbers firstrow">2</div>
+                        <div class="numbers secondrow">15</div>
+                        <div class="numbers thirdrow">5</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('semantic/semantic.js') }}"></script>
+
+{{--<script src="js/js.js"></script>--}}
