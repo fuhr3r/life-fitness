5dff53c96f72cefdb646ea86612f107a140da315
lelukas <lelukass@gmail.com>
2017-08-30 12:24:49 -0300

    created flex grid for training view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,12 @@
-
-
 <head>
     <title>Academia Life Fitness</title>
-    <meta  charset="UTF-8" />
-    <meta name=”viewport” content=”width=device-width, user-scalable=0; initial-scale=1.0, maximum-scale=1.0" >
-    <meta name=”viewport” content=”width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=false;” >
-    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <meta charset="UTF-8"/>
+    <meta name=”viewport” content=”width=device-width, user-scalable=0; initial-scale=1.0, maximum-scale=1.0">
+    <meta name=”viewport” content=”width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=false;”>
+    <meta name="viewport" content="width=device-width, user-scalable=no"/>
 
-    <link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css/app.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}"/>
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css/app.css') }}"/>
 </head>
 
 <div class="ui left demo thin vertical inverted sidebar labeled icon menu">
@@ -31,7 +29,7 @@
 </div>
 
 <div class="pusher">
-
+    {{--<img class="ui mini image" src="{{ URL::asset('images/icons/ombro.png') }}">--}}
     <div class="ui fixed inverted menu">
         <div class="ui container">
             <a id="menuButton" href="#" class="item circular ui icon button">
@@ -44,30 +42,116 @@
         </div>
     </div>
 
-    <section class="ui padded grid">
+    <section class="ui padded container">
 
-        <div class="ui items">
-            <div class="item">
-                <h2 class="ui header">
-                    <img class="ui mini image" src="{{ URL::asset('images/icons/ombro.png') }}">
-                    <div class="content">
-                        Ombro
-                    </div>
-                </h2>
 
-                <div class="item">
-                    <p>Supino Reto</p>
-                    <div class="right floated item">
-                        2
-                    </div>
+        <div class="left container">
+            <div class="heading wrapper">
+                <img class="ui mini image" src="{{ URL::asset('images/icons/ombro.png') }}">
+                <h2>Ombro</h2>
+            </div>
+
+            <div class="trainings list">
+                <span>Lorem Lorem Lorem</span>
+            </div>
+        </div>
+
+
+
+
+        <div class="right container">
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
                 </div>
-                <p>Supino Reto</p>
-                <p>Supino Reto</p>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
+            </div>
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
+                </div>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
+            </div>
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
+                </div>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
             </div>
 
         </div>
 
 
+    </section>
+
+    <section class="ui padded container">
+
+
+        <div class="left container">
+            <div class="heading wrapper">
+                <img class="ui mini image" src="{{ URL::asset('images/icons/ombro.png') }}">
+                <h2>Ombro</h2>
+            </div>
+
+            <div class="trainings list">
+                <span>Lorem Lorem Lorem</span>
+            </div>
+        </div>
+
+
+
+
+        <div class="right container">
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
+                </div>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
+            </div>
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
+                </div>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
+            </div>
+
+            <div class="serie column">
+                <div class="icon wrapper">
+                    <i class="undo icon"></i>
+                </div>
+
+                <div class="values list">
+                    <span>99</span>
+                </div>
+
+            </div>
+
+        </div>
 
 
     </section>
@@ -75,7 +159,8 @@
 </div>
 
 
-<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('semantic/semantic.js') }}"></script>
 <script src="{{ URL::asset('js/app.js') }}"></script>
 
