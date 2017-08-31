faffef7d32fa64a0ee21b292f816b10a7aed01c5
lelukas <lelukass@gmail.com>
2017-08-31 17:50:57 -0300

    Icons for training view Swipe to open sidebar

--- public/js/app.js
+++ public/js/app.js
@@ -2,51 +2,42 @@ $('#menuButton').click(function(event) {
     $('.ui.sidebar').sidebar('toggle');
 });
 
-$('#home').click(function(event) {
-    var data = "\webapp.html"
+var resim = $("body");
 
-    window.location.href=data;
+resim.hammer().on("swiperight", function(ev) {
+    $('.ui.sidebar').sidebar('toggle');
 });
 
-$('#digievolution').click(function(event) {
-    var data = "\digievolution.html"
 
-    window.location.href=data;
-});
-
-$('#quit').click(function(event) {
-    window.location.href='\index.html';
-});
-
-$(document).ready(function() {
-    $('.ui.form').form({
-        fields: {
-            CPF: {
-                identifier  : 'CPF',
-                rules: [
-                    {
-                        type   : 'empty',
-                        prompt : 'Por favor, digite seu CPF'
-                    },
-                    {
-                        type   : 'empty',
-                        prompt : 'CPF inválido'
-                    }
-                ]
-            },
-            password: {
-                identifier  : 'password',
-                rules: [
-                    {
-                        type   : 'empty',
-                        prompt : 'Por favor, digite sua senha'
-                    },
-                    {
-                        type   : 'length[6]',
-                        prompt : 'Sua senha deve conter no mínimo 6 dígitos'
-                    }
-                ]
-            }
-        }
-    });
-});
+// $(document).ready(function() {
+//     $('.ui.form').form({
+//         fields: {
+//             CPF: {
+//                 identifier  : 'CPF',
+//                 rules: [
+//                     {
+//                         type   : 'empty',
+//                         prompt : 'Por favor, digite seu CPF'
+//                     },
+//                     {
+//                         type   : 'empty',
+//                         prompt : 'CPF inválido'
+//                     }
+//                 ]
+//             },
+//             password: {
+//                 identifier  : 'password',
+//                 rules: [
+//                     {
+//                         type   : 'empty',
+//                         prompt : 'Por favor, digite sua senha'
+//                     },
+//                     {
+//                         type   : 'length[6]',
+//                         prompt : 'Sua senha deve conter no mínimo 6 dígitos'
+//                     }
+//                 ]
+//             }
+//         }
+//     });
+// });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
     <meta name=”viewport” content=”width=device-width, user-scalable=0; initial-scale=1.0, maximum-scale=1.0">
     <meta name=”viewport” content=”width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=false;”>
     <meta name="viewport" content="width=device-width, user-scalable=no"/>
+    <meta name="theme-color" content="#000000" />
 
+    {{--<link rel="icon" sizes="192x192" href="nice-highres.png"> //logo--}}
     <link rel="stylesheet" href="{{ URL::asset('semantic/semantic.css') }}"/>
     <link rel="stylesheet" href="{{ URL::asset('css/app.css/app.css') }}"/>
 </head>
@@ -15,15 +17,15 @@
         <span>Treino de Hoje</span>
     </a>
     <a class="item">
-        <i class="line chart icon"></i>
+        <img class="ui mini image" src="{{ URL::asset('images/icons/sidebar/graph.svg') }}">
         <span>Minha Evolução</span>
     </a>
     <a class="item">
-        <i class="setting icon"></i>
+        <img class="ui mini image" src="{{ URL::asset('images/icons/sidebar/wrench.svg') }}">
         <span>Configurações</span>
     </a>
     <a class="item">
-        <i class="sign out icon"></i>
+        <img class="ui mini image" src="{{ URL::asset('images/icons/sidebar/log-out.svg') }}">
         <span>Sair</span>
     </a>
 </div>
@@ -37,7 +39,7 @@
             </a>
             <div class="header item disabled">
                 {{--<img class="ui mini circular image konata" src="images/maxresdefault.jpg">--}}
-                <span class="main title">Treinos de hoje</span>
+                <span class="main title">Treino de hoje</span>
             </div>
         </div>
     </div>
@@ -47,10 +49,10 @@
         <div class="left container">
             <div class="heading wrapper">
                 <img class="ui mini image" src="{{ URL::asset('images/icons/ombro.png') }}">
-                <h2>Ombro</h2>
+                <h2>Posterior da coxa</h2>
             </div>
             <div class="trainings list">
-                <span>Lorem Lorem Lorem</span>
+                <span>Rosca alternada B. inclinada</span>
                 <span>Lorem Lorem Lorem</span>
             </div>
         </div>
@@ -149,6 +151,12 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('semantic/semantic.js') }}"></script>
+
+<script src="{{ URL::asset('js/hammer.min.js') }}"></script>
+<script src="{{ URL::asset('js/hammer-time.min.js') }}"></script>
+<script src="{{ URL::asset('js/jquery.hammer.js') }}"></script>
+
+
 <script src="{{ URL::asset('js/app.js') }}"></script>
 
 {{--<script src="js/js.js"></script>--}}
