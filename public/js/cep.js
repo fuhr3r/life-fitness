cb7c851026aff20c456ed09398cdf1c322552583
lelukas <lelukass@gmail.com>
2017-09-16 20:02:27 -0300

    administrator view

--- public/js/cep.js
+++ public/js/cep.js
@@ -1,30 +1,31 @@
+$("input[name=cep]").one("mousedown focus", function (e) { //TODO: duplicated return when clicked
 
-
-$("input[name=cep]").on("click focus", function (e) {
-    e.stopPropagation();
     $(this).keypress(function () {
         setTimeout(() => {
-            console.log(this.value.length);
-            if(this.value.length == 9){
+            if (this.value.length == 9) {
                 let cep = (this.value).replace(/-/g, "");
-                // console.log(cep);
                 getAddress(cep)
             }
         }, 100)
     })
+
 });
 
 
 function getAddress(cep) {
-        $.get("http://viacep.com.br/ws/"+cep+"/json/", function (cep) {
-           console.log(cep);
-           if(!cep.erro){
-               $("input[name=street]").val(cep.logradouro);
-               $("input[name=neighborhood]").val(cep.bairro);
-               $("input[name=city]").val(cep.localidade);
-               $("input[name=state]").val(cep.uf);
+    $.get("http://viacep.com.br/ws/" + cep + "/json/", function (cep) {
+        console.log(cep);
+        if (!cep.erro) {
+            $("input[name=street]").val(cep.logradouro);
+            $("input[name=neighborhood]").val(cep.bairro);
+            $("input[name=city]").val(cep.localidade);
+            $("input[name=state]").val(cep.uf);
 
-               $("input[name=number]").focus();
-           }
-        });
+            $("input[name=number]").focus(function () {
+                if (!this.val() == "") {
+                    this.val("");
+                }
+            });
+        }
+    });
 }
