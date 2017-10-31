775ad609ce67340fb88b532c91ea2038d9d62787
Bruno Brito <bruno.brito.ln@gmail.com>
2017-10-31 19:52:59 -0300

    check db credentials

--- config/database.php
+++ config/database.php
@@ -2,10 +2,11 @@
 
 $url = parse_url(getenv("DATABASE_URL"));
 
-$host = is_null($url["host"]) ? '' : $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$host = is_null($url["host"]) ? '127.0.0.1' : $url["host"];
+$username = is_null($url["user"]) ? 'forge' : $url["user"];
+$password = is_null($url["pass"]) ? '' : $url["pass"];
+$db = substr($url["path"], 1);
+$database = is_null($db) ? 'forge' : $db;
 
 return [
 
@@ -61,11 +62,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => is_null($url["host"]) ? '127.0.0.1' : $url["host"],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => is_null(substr($url["path"], 1)) ? 'forge' : substr($url["path"], 1),
-            'username' => is_null($url["user"]) ? 'forge' : $url["user"],
-            'password' => is_null($url["pass"]) ? '' : $url["pass"],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
